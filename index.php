9cc120e0f734fa9551ce3800a665819a2a752297
Chris Kovach <cckovach@gmail.com>
2013-03-09 16:56:38 -0500

    Updated to add Script Reference for me3d.world.js which handles app states.

--- index.php
+++ index.php
@@ -31,6 +31,7 @@
         <script src="js/meui.js"></script>
         <script src="js/me3d.scene.js"></script>
         <script src="js/me3d.physics.js"></script>
+        <script src="js/me3d.population.js"></script>
         <script src="js/me3d.emitter.js"></script>
         <script src="js/me3d.sparks.js"></script>
         <script src="js/me3d.picker.js"></script>
@@ -38,9 +39,12 @@
         <script src="js/me3d.stage.js"></script>
         <script src="js/me3d.grid.js"></script>
         <script src="js/me3d.avatar.js"></script>
+        <script src="js/me3d.avatarmover.js"></script>
         <script src="js/me3d.builder.js"></script>
         <script src="js/me3d.controls.js"></script>
+        <script src="js/me3d.transitions.js"></script>
         <script src="js/me3d.render.js"></script>
+        <script src="js/me3d.world.js"></script>
         <script src="js/main.js"></script>
     </body>
 </html>
