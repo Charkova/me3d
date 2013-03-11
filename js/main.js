c6b79c0a885e9313adf112b09eaedec2bf5d7cd4
Chris Kovach <cckovach@gmail.com>
2013-03-10 21:21:00 -0400

    moved some funcitonality to me3d.world.js and cleaned up remaining code

--- js/main.js
+++ js/main.js
@@ -1,149 +1,117 @@
 $(document).ready(function(){
 	
-	// default scene properties
-	///////////////////////////////////
-	var	clock = new THREE.Clock();		
+	/* 
+	 * start world which includes: default scenes for city,
+	 * zone(interior), and map, renderer, and physics;
+	 * 
+	 * methods for initializing and switching scenes;
+	 * 
+	 * will be expanded to include managers/observers;
+	 */
 	
-	// main scene
-	///////////////////////////////////	
-	var myWorld = new ME3D.Scene();
+	var MEAPP = new ME3D.World();
 	
-	// init physics
-	///////////////////////////////////	
-	var myPhysics = new ME3D.Physics();
-	var myBounds = new ME3D.Bounds();
+	// LOCAL REFERENCES //
+	//////////////////////
+	var CITY = MEAPP.cityView,
+		ZONE = MEAPP.zoneView,
+		MAP = MEAPP.mapView,
+		RENDERER = MEAPP.renderer,
+		PHYSICS = MEAPP.physics,
+		BOUNDS = MEAPP.bounds,
+		EMITTER = MEAPP.emitter,
+		CLOCK = MEAPP.clock;
+		ELEMENT = RENDERER.element;
 	
-		
+	MEAPP.preload();
 	
-	// environment
-	///////////////////////////////////
-	var myStage = new ME3D.Stage();
-	myWorld.scene.add(myStage.getStage());
-	//myPhysics.addStiffBody(myStage.getBase());
-	console.log(myBounds.getBoundsMin(myStage.getBase()));
-	console.log(myBounds.getBoundsMax(myStage.getBase()));
-	var stageBounds = myBounds.getBoundsMax(myStage.getBase());	
-	myPhysics.addCalcStiffBody(myStage.getBase(),stageBounds);
-	//var myBounds = new ME3D.Bounds();
-	//var baseBody = myBounds.computePlane(myStage.getBase());
-	//myPhysics.addPremadeBody();
+	// GLOBAL EVENT LISTENERS //
+	////////////////////////////
+	//ELEMENT.addEventListener( 'keydown', bind( this, this.onKeyDown ), false );
 	
 	
-	// grid
-	///////////////////////////////////
-	var myGrid = new ME3D.Grid(2,.5,11);
-	myWorld.scene.add(myGrid.getGrid());
-	var gridChildren = myGrid.getChildren();
+	// INITIALIZE CITYVIEW ENVIRONMENT //
+	/////////////////////////////////////
+	var cityStage = new ME3D.Stage();
+	var cityStageBounds = BOUNDS.getBoundsMax(cityStage.getBase());	
 	
+	CITY.scene.add(cityStage.getStage());
+	PHYSICS.addCalcStiffBody(cityStage.getBase(),cityStageBounds);
+	
+	
+	// INITIALIZE CITYVIEW GRID //
+	//////////////////////////////
+	var cityGrid = new ME3D.Grid(2,.5,11);
+	var gridChildren = cityGrid.getChildren();
+
+	// do physics for grid squares	
 	for(var i=0,j=gridChildren.length; i<j; i++){
-		var gridSquareBounds = myBounds.getBoundsMin(gridChildren[i]);
-		myPhysics.addMassBody(gridChildren[i],gridSquareBounds,0);		
+		var gridSquareBounds = BOUNDS.getBoundsMin(gridChildren[i]);
+		PHYSICS.addMassBody(gridChildren[i],gridSquareBounds,0);		
 	};
 	
+	CITY.scene.add(cityGrid.getGrid());
 	
-	// avatar
-	///////////////////////////////////
+	// INITIALIZE AVATAR //
+	///////////////////////
 	var myAvatar = new ME3D.Avatar(new THREE.Vector3(1,1,1));
-	myWorld.scene.add(myAvatar.getAvatar());
-	console.log(myBounds.getBoundsMax(myAvatar.getBoundsMesh()));
-	var avatarBounds = myBounds.getBoundsMax(myAvatar.getBoundsMesh());
-	avatarBounds.multiplyScalar(2);
-	console.log(avatarBounds);
-	var avatarController = myPhysics.addMassBody(myAvatar.getAvatar(),avatarBounds,3);	
-	//var avatarController = myPhysics.addRigidBody(myAvatar.getAvatar());
+	CITY.scene.add(myAvatar.getAvatar());
+	var myAvatarBounds = BOUNDS.getBoundsMax(myAvatar.getBoundsMesh());
+	myAvatarBounds.multiplyScalar(1.8);
+	var myAvatarController = PHYSICS.addMassBody(myAvatar.getAvatar(),myAvatarBounds,3);
 	
-		
+	// INITIALIZE TEST PHYSICS CUBE //
+	//////////////////////////////////
+	var cubeMap = THREE.ImageUtils.loadTexture("textures/boxStripe.gif");	
+	var testCube = new THREE.Mesh(
+		new THREE.CubeGeometry(1,1,1),
+		new THREE.MeshLambertMaterial({
+			map: cubeMap, emissive:0x007777,
+			opacity: .75, transparent:true,
+			side:THREE.DoubleSide}));
 	
-	// STUPID CUBE TEST
-	///////////////////
-	var geo = new THREE.CubeGeometry(1,1,1);
-	var mat = new THREE.MeshNormalMaterial({side:THREE.DoubleSide});
-	var cube = new THREE.Mesh(geo,mat);
-	
-	cube.useQuaternion = true;
+	testCube.useQuaternion = true;
 	var quaternion = new THREE.Quaternion();
 	quaternion.setFromAxisAngle( new THREE.Vector3( 1, 0, 0 ), ME3D.de2ra(-45) );
-    cube.quaternion.copy(quaternion);
-   	cube.position.y = 4;
+    testCube.quaternion.copy(quaternion);
+   	testCube.position.y = 4;
    	
-    myWorld.scene.add(cube);
-    console.log(myBounds.getBoundsMax(cube));
-	var cubeBounds = myBounds.getBoundsMax(cube);	
-	myPhysics.addMassBody(cube,cubeBounds,.1);
-   	//var cubeController = myPhysics.addRigidBody(cube);
-	///////////////////
+	var cubeBounds = BOUNDS.getBoundsMax(testCube);	
+	PHYSICS.addMassBody(testCube,cubeBounds,.1);
 	
-	// emitter
-	///////////////////////////////////
-	var myEmitter = new ME3D.Emitter();
-	myAvatar.avatar.add(myEmitter.getSystem());
+	CITY.scene.add(testCube);
 	
-	// BUILDINGS!!
-	///////////////////////////////////
+	// BUILDINGS!! //
+	/////////////////
 	//myBuilder = new ME3D.Builder(myWorld.scene, myPhysics);
  	//myBuilder.makeBuilding(4, new THREE.Vector3(1,1,1), new THREE.Vector3(-2.5,0,0), 'City Hall');
-	
-
-	// PICKING!!
-	///////////////////////////////////
-	var myPicker = new ME3D.Picker(myWorld.scene, myWorld.camera);
+ 	
+ 	// PICKING!! //
+	///////////////
+	var PICKER = new ME3D.Picker(CITY.scene, CITY.camera);
 		
 		
-	// controls	
-	///////////////////////////////////
-	var controlsA = new ME3D.AvatarControls(avatarController, myAvatar.avatar);
-	//var controlsC = new ME3D.CameraControls(myWorld.camera);
-	//var controls = new ME3D.AvatarControls(cube);
+	// CONTROLS //	
+	//////////////
+	var controlsA = new ME3D.AvatarControls(myAvatarController, myAvatar.avatar);
 	
-	
-	
-	// avatar location function hack
-	///////////////////////////////////
-	var oldAvatarLocation;
-	
-	var showAvatarLoc = function() {
-		var value = myAvatar.getPosition();
-		
-		if(!$('#location').length) {
-			$('body').append('<div id="location" style="position:absolute; top:50px; left:10px; font-weight:bold; color:#FFFFFF"></div>');
-		}
-		
-		if(typeof value !== 'undefined') {
-			$('#location').empty();
-			$('#location').text(value.x + ', '+ value.y + ', ' + value.z);
-			
-		}
-	} // now adding this function to my loop below
-	
-	
-	// UGH, NO WAY AROUND THIS RIGHT NOW...
-	var resolveControls = function() {
-		var delta = clock.getDelta();
+	// RENDERER //
+	//////////////
+	var renderLoop = function() {
+		var delta = CLOCK.getDelta();
 	    controlsA.update( delta );
-	    //controlsC.update( delta );
-	    //myAvatar.bindCamera(myWorld.camera);
-	    myWorld.camera.position.x = myAvatar.avatar.getChildPosition('cameraTarget').x;
-	    myWorld.camera.position.y = myAvatar.avatar.getChildPosition('cameraTarget').y;
-	    myWorld.camera.position.z = myAvatar.avatar.getChildPosition('cameraTarget').z;
-	    //myWorld.camera.position.z = myAvatar.avatar.position.z;
-	    //console.log(myAvatar.avatar.getChildPosition('cameraActual'));
-	    myEmitter.update(delta);
-	    myPicker.pick();
-	    myPicker.pickBuilding();
-	    showAvatarLoc();	    
+	    CITY.camera.position.x = myAvatar.avatar.getChildPosition('cameraTarget').x;
+	    CITY.camera.position.y = myAvatar.avatar.getChildPosition('cameraTarget').y;
+	    CITY.camera.position.z = myAvatar.avatar.getChildPosition('cameraTarget').z;
+	    
+	    PICKER.pick();
+	    PICKER.pickBuilding();
+	    //showAvatarLoc();	  
+	    //myTransitions.fadeOut(delta);
+	    ME3D.Ticker.run(); 
 	}
 	
-	// debug
-	///////////////////////////////////
-	myStage.log();
-	console.log(myAvatar.avatar);
-	console.log(myAvatar.getPosition());
-	
-
-	// renderer
-	///////////////////////////////////
-	var myRenderer = new ME3D.Render(myWorld.scene, myWorld.camera);
-	myRenderer.queueRender(resolveControls,'');
-	myRenderer.queueAnimation(runPhysics,'');
+	RENDERER.queueRender(renderLoop,'');
+	RENDERER.queueAnimation(runPhysics,'');
 
 });
